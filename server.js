a6737002e18c679cb38cc27f23075a27d73c278e
pbryzek <bryzek@gmail.com>
2022-07-12 17:10:25 -0700

    update APIs

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const uri = process.env.MONGODB_URI;
 const babcoin_db = "babcoin";
 const collection_users = "users";
 const collection_events = "events";
-console.log("Server Deployed!");
+const collection_userevents = "userevents";
 
 // use the express-static middleware
 app.use(express.static("public"));
 
-// B@B User API
+//// Users
+// B@B Users API
+////
 app.get("/v1/users", async function (req, res) {
   const client = await MongoClient.connect(uri, { 
     useNewUrlParser: true, 
@@ -24,20 +26,23 @@ app.get("/v1/users", async function (req, res) {
     return res.json(users);
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
   }
 });
-
+//// Users
+// B@B User API
+////
 app.get("/v1/user/:id", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
   try {
-    await client.connect();
-
-    const database = client.db(db);
-    const users = database.collection(collection_users);
+    const db = client.db(babcoin_db);  
+    const users = db.collection(collection_users);
     var o_id = new ObjectID(req.params.id);
     users.findOne({'_id':o_id})
     .then(function(doc) {
@@ -48,6 +53,7 @@ app.get("/v1/user/:id", async function (req, res) {
     });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
@@ -56,14 +62,23 @@ app.get("/v1/user/:id", async function (req, res) {
 
 // Get all events a user has gone to
 app.get("/v1/userevents/:id", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
   try {
-    await client.connect();
-    const database = client.db(db);
-    const users = database.collection(collection_users).find().toArray();
-    return res.json(users);
+    const db = client.db(babcoin_db);
+    var o_id = new ObjectID(req.params.id);
+    const userevents = db.collection(collection_userevents).find().toArray();
+    userevents.find({'user_id':o_id}).then(function(doc) {
+      if(!doc)
+        throw new Error('No record found.');
+      console.log(doc);
+      return res.json(doc);
+    });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
@@ -72,18 +87,23 @@ app.get("/v1/userevents/:id", async function (req, res) {
 
 // Get specific event object
 app.get("/v1/event/:id", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });  
   try {
-    await client.connect();
-
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
-    const movie = await cursor.next();
-
-    return res.json(movie);
+    const db = client.db(babcoin_db);
+    const events = db.collection(collection_events);
+    var o_id = new ObjectID(req.params.id);
+    events.find({'user_id':o_id}).then(function(doc) {
+      if(!doc)
+        throw new Error('No record found.');
+      console.log(doc);
+      return res.json(doc);
+    });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
@@ -92,18 +112,23 @@ app.get("/v1/event/:id", async function (req, res) {
 
 // Get subset of event object for nft metadata
 app.get("/v1/nft/:id", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });  
   try {
-    await client.connect();
-
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
-    const movie = await cursor.next();
-
-    return res.json(movie);
+    const db = client.db(babcoin_db);
+    const events = db.collection(collection_events);
+    var o_id = new ObjectID(req.params.id);
+    events.find({'user_id':o_id}).then(function(doc) {
+      if(!doc)
+        throw new Error('No record found.');
+      console.log(doc);
+      return res.json(doc);
+    });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
@@ -112,11 +137,13 @@ app.get("/v1/nft/:id", async function (req, res) {
 
 // Get all events
 app.get("/v1/events", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });  
   try {
-    await client.connect();
-    const database = client.db(db);
-    const events = database.collection(collection_users).find().toArray();
+    const db = client.db(babcoin_db);    
+    const events = await db.collection(collection_events).find({}).toArray();
     return res.json(events);
   } catch(err) {
     console.log(err);
@@ -128,26 +155,97 @@ app.get("/v1/events", async function (req, res) {
 
 // Create a user
 app.post("/v1/user", async function (req, res) {
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
+  const f_name = req.params.first_name;
+  const l_name = req.params.last_name;
+  const u_email = req.params.email;
+  const u_wallet_address = req.params.wallet_address;
+  const u_role = req.params.role;
+  
+  if(!f_name) {
+    return res.json({message: "Missing Required param first_name!"});
+  }
+  if(!l_name) {
+    return res.json({message: "Missing Required param last_name!"});
+  }
+  if(!u_email) {
+    return res.json({message: "Missing Required param email!"});
+  }
+  if(!u_wallet_address) {
+    return res.json({message: "Missing Required param wallet_address!"});
+  }
+  if(!u_role) {
+    return res.json({message: "Missing Required param role!"});
+  }
+  var userObj = { first_name: f_name, last_name: l_name, email: u_email, wallet_address: u_wallet_address, role: u_role };
+
   try {
-    await client.connect();
-    // Proper return
-    return res.json({});
+    const db = client.db(babcoin_db);
+    const users = db.collection(collection_users);
+    users.insertOne(userObj, function(err, res) {
+      if (err) throw err;
+      return res.json(res);
+    });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
+    await db.close();
     await client.close();
   }
 });
 
 // Create an event
 app.post("/v1/event", async function (req, res) {
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
+  const start_ts = req.params.start_timestamp;
+  const end_ts = req.params.end_timestamp;
+  const e_name = req.params.name;
+  const e_type = req.params.type;
+  const e_role = req.params.role;
+  const e_image_url = req.params.image_url;
+  const e_qrcode_url = req.params.qrcode_url;
+  
+  if(!start_ts) {
+    return res.json({message: "Missing Required param start_timestamp!"});
+  }
+  if(!end_ts) {
+    return res.json({message: "Missing Required param end_timestamp!"});
+  }
+  if(!e_name) {
+    return res.json({message: "Missing Required param name!"});
+  }
+  if(!e_type) {
+    return res.json({message: "Missing Required param type!"});
+  }
+  if(!e_role) {
+    return res.json({message: "Missing Required param role!"});
+  }
+  if(!e_image_url) {
+    return res.json({message: "Missing Required param image_url!"});
+  }
+  if(!e_qrcode_url) {
+    return res.json({message: "Missing Required param qrcode_url!"});
+  }
+  var eventObj = { start_timestamp: start_ts, end_timestamp: end_ts, name: e_name, type: e_type, role: e_role, image_url: e_image_url, qrcode_url: e_qrcode_url };
+
   try {
-    await client.connect();
-    // Proper return
-    return res.json({});
+    const db = client.db(babcoin_db);
+    const events = db.collection(collection_events);
+    events.insertOne(eventObj, function(err, res) {
+      if (err) throw err;
+      return res.json(res);
+    });
   } catch(err) {
     console.log(err);
+    return res.json(err);
   }
   finally {
     await client.close();
